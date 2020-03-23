d3c50f69ca95a68c730791fc1b8b0f9436e8e100
Dave Pernitz <pernitz@uwalumni.com>
2020-03-23 16:22:30 -0600

    Add Ingredient class

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,1 +1,14 @@
+class Ingredient {
+  constructor(id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price
+  }
+}
 
+
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Ingredient;
+}
