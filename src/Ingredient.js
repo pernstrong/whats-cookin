147cbc14c5e9eab120669b3314fea920a223e9b3
Dave Pernitz <pernitz@uwalumni.com>
2020-03-23 18:49:02 -0600

    Add pantry test and class

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,13 +1,21 @@
 class Ingredient {
-  constructor(id, name, price) {
+  constructor(id, name, price, amount) {
     this.id = id;
     this.name = name;
-    this.price = price
+    this.price = price;
+    this.amount = amount;
   }
+  // cant decide on add in ingredient or pantry...
+  // leaning towards amount here, but add/remove in pantry
+  // add(amountAdded) {
+  //   this.amount += amountAdded;
+  // }
+  //
+  // use(amountUsed) {
+  //   this.amount -= amountUsed;
+  // }
+
 }
-
-
-
 
 if (typeof module !== 'undefined') {
   module.exports = Ingredient;
--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -10,10 +10,10 @@ describe('Ingredient', function() {
 
   beforeEach(function() {
 
-    ingredient1 = new Ingredient(20081, "wheat flour", 142)
-    ingredient2 = new Ingredient(18372, "bicarbonate of soda", 582)
-    ingredient3 = new Ingredient(1123, "eggs", 472)
-    ingredient4 = new Ingredient(4543522, 'beer', 1000)
+    ingredient1 = new Ingredient(20081, "wheat flour", 142, 5)
+    ingredient2 = new Ingredient(18372, "bicarbonate of soda", 582, 20)
+    ingredient3 = new Ingredient(1123, "eggs", 472, 0)
+    ingredient4 = new Ingredient(4543522, 'beer', 1000, 200)
 
   })
 
@@ -44,6 +44,35 @@ describe('Ingredient', function() {
     expect(ingredient1.price).to.equal(142);
     expect(ingredient4.price).to.equal(1000)
   })
+
+  it('should have an amount', function() {
+
+    expect(ingredient2.amount).to.equal(20)
+    expect(ingredient4.amount).to.equal(200)
+  })
+
+  // it('can add to to the amount', function() {
+  //
+  //   expect(ingredient1.amount).to.equal(5)
+  //   expect(ingredient3.amount).to.equal(0)
+  //
+  //   ingredient1.add(15)
+  //   ingredient3.add(5)
+  //
+  //   expect(ingredient1.amount).to.equal(20)
+  //   expect(ingredient3.amount).to.equal(5)
+  // })
+  //
+  // it('can be used', function() {
+  //   expect(ingredient1.amount).to.equal(5)
+  //   expect(ingredient4.amount).to.equal(200)
+  //
+  //   ingredient1.use(3)
+  //   ingredient4.use(101)
+  //
+  //   expect(ingredient1.amount).to.equal(2)
+  //   expect(ingredient4.amount).to.equal(99)
+  // })
 
 
 })
